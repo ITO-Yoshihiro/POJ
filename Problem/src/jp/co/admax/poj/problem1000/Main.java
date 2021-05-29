0754201ff5c707c359a6b8671b4fc4ea0095dfef
ito0032 <ito.yoshihiro@ad-max.co.jp>
2021-05-30 08:56:55 +0900

    問題1000の追加

---
+++ Problem/src/jp/co/admax/poj/problem1000/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Main.java
+ *
+ * 2021/05/29 ito.y
+ *
+ * Copyright (c)2021, ADMAX Corporation. All rights reserved.
+ */
+package jp.co.admax.poj.problem1000;
+
+import java.util.Scanner;
+
+/**
+ * Problem No.1000
+ * A+B Problem
+ * 
+ * @author ito.y
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		Scanner cin = new Scanner(System.in);
+		int a = cin.nextInt();
+		int b = cin.nextInt();
+		System.out.println(a+b);
+		cin.close();
+	}
+
+}
